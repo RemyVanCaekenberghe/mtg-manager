9650fe522e2ecf9adb636ec1815e48074522164a
Rémy Van Caekenberghe <remyvancaekenberghe@gmail.com>
2019-12-05 21:43:13 +0100

    Update app name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('news app is running!');
+    expect(page.getTitleText()).toEqual('mtg-manager app is running!');
   });
 
   afterEach(async () => {
